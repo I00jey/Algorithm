f1aba82719f89acb6f51128dcd00c4750e9a78f1
I00jey <cyj5056@gmail.com>
2024-04-23 23:35:13 +0900

    baekjoon 1260 update

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,32 +1,42 @@
-# 노드, 간선, 시작 노드 입력 받기
-n, m, start = list(map(int,input().split(" ")))
+from collections import deque
 
+def dfs(start):     # 재귀
+    print(start, end=" ")
+    visit[start] = 1
 
-# 그래프 간선 저장 리스트
-lines = []
+    for i in range(1, n+1):
+        if arr[i][start] == 1 and not visit[i]:
+            dfs(i)
+
+    return
+
+def bfs(start):     # queue
+    que = deque()
+    que.append(start)
+    visit[start] = 1
+
+    print(start, end=" ")
+
+    while que:
+        top = que.popleft()
+
+        for i in range(1, n+1):
+            if not visit[i] and arr[i][top] == 1:
+                que.append(i)
+                visit[i] = 1
+                print(i, end=" ")
+
+    return
+
+n, m, v = map(int, input().split(" "))
+visit = [0 for _ in range(n+1)]  # 노드 방문 기록 -> [0,1,1,0]
+arr = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
 for _ in range(m):
-    # 간선 수 만큼 간선 입력 받기
-    lines.append(list(map(int, input().split(" "))))
+    a, b = map(int, input().split(" "))
+    arr[a][b] = arr[b][a] = 1
 
-nodes = []
-for i in range(1, n+1):
-    nodes.append(i)
-
-
-# 깊이 우선 탐색 DFS
-def deep_first():
-
-    print()
-
-
-# 너비 우선 탐색 BFS
-def breadth_first():
-    result = [start]
-    for line in lines:
-        if start in
-    print(*result)
-
-
-deep_first()
-breadth_first()
-
+dfs(v)
+print()
+visit = [0 for _ in range(n+1)]  # 노드 방문 기록 -> [0,1,1,0]
+bfs(v)
