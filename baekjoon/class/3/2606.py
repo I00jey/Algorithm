ee235c8bb6fd2129b23dcb45564534f9cf099d68
I00jey <cyj5056@gmail.com>
2024-12-04 22:04:48 +0900

    solved.ac class 2

--- baekjoon/class/3/2606.py
+++ baekjoon/class/3/2606.py
@@ -1,6 +1,6 @@
 # 바이러스
 
-# # 내 풀이
+# 내 풀이
 # from collections import deque
 #
 # # 컴퓨터의 수
@@ -49,8 +49,9 @@ for _ in range(edge):
     graph[a].append(b)
     graph[b].append(a)
 
-# BFS 탐색
 visitied = set()
+
+# BFS 탐색
 queue = deque([1])
 while queue:
     current = queue.popleft()
@@ -62,6 +63,18 @@ while queue:
         queue.extend(graph[current])
     # print('현재까지 감염된 컴퓨터 : ', visitied)
     # print('현재 연결 컴퓨터 : ', queue)
+
+# DFS 탐색
+stack = [1] # 깊이우선탐색은 스택 사용
+while stack:
+    current = stack.pop()
+    # 감염되지 않은 컴퓨터일 경우
+    if current not in visitied:
+        # 감염된 컴퓨터 추가
+        visitied.add(current)
+        # 감염된 컴퓨터와 연결된 컴퓨터들을 스택에 추가
+        stack.extend(graph[current])
+
 
 # 1을 제외한 감염된 컴퓨터의 수
 print(len(visitied)-1)
